1dd755a91f94c0d6a7cff384520f49a8f6514aa6
Ant. Lucas Vieira de Lima <lucaslima14072003@gmail.com>
2022-12-19 09:49:11 -0300

    Aula 10
    Algumas anotações a mais sobre a aula 9 e aula 10 completa com funções de js

---
+++ Aula10/funcoes.js
@@ -0,0 +1,11 @@
+function clicar(){
+    a.innerText = 'Você clicou aqui'
+    a.style.background = 'red'
+}
+function entrar(){
+    a.innerText = 'O mouse entrou na area'
+}
+function sair(){
+    a.innerText = 'O mouse saiu'
+    a.style.background = 'green'
+}
